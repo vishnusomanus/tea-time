fad959a4accaf18624155a895e1518cdd6460ab5
Vishnu Soman <vishnusoman@Vishnus-MacBook-Air.local>
2023-02-27 19:02:39 +0530

    google loo change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,8 +36,8 @@
         <hr>
 
         <div class="flex items-center justify-end mt-4">
-            <a href="{{ url('login/google') }}">
-                <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png">
+            <a class="btn"href="{{ url('login/google') }}">
+                <img src="https://vishnusoman.com/tea-time/public/images/google.png" width="40px">
             </a>
             <a class="btn btn-primary" href="{{ url('register') }}">Register</a>
         </div>
